0cd8e498aae6870ae07a058034f565159fa26d23
Taha-KK <115929771+Taha-DD@users.noreply.github.com>
2024-06-26 11:25:36 +0100

    Add files via upload

---
+++ ValentinePrankV2.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+
+int main() {
+    char alpha[] = "1abcdefghijklmnopqrstuvwxyz? ABCDEFGHIJKLMNOPQRSTUVWXYZ<3";
+    char love[] = "I Love U Cute head will you be my Valentine <3?";
+    char kidding[] = "Just kidding no Valentine for you homie XD      ";
+
+    for (char *ptr = love; *ptr != '\0'; ptr++) {
+        char *p = alpha;
+        while (*p != *ptr) {
+            printf("\b%c", *(++p));
+            fflush(stdout);
+            usleep(1e4);
+        }
+        printf("%c", *p);
+        fflush(stdout);
+        usleep(110000);
+    }
+    printf("\r");
+    for (char *ptr = kidding; *ptr != '\0'; ptr++) {
+        char *p = alpha;
+        while (*p != *ptr) {
+            printf("\b%c", *(++p));
+            fflush(stdout);
+            usleep(1e4);
+        }
+        printf("%c", *p);
+        fflush(stdout);
+        usleep(110000);
+    }
+
+    return 0;
+}
